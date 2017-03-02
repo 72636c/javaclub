1578decf0b9f46348e93fff121902b3f3ee40c0c
Ryan Ling <rling-seek@users.noreply.github.com>
2017-03-03 09:32:04 +1100

    Add theme switching without reload

--- public/assets/common/js/script.js
+++ public/assets/common/js/script.js
@@ -2,7 +2,7 @@ function changeTheme(theme) {
     var expiry_date = new Date();
     expiry_date.setTime(expiry_date.getTime() + (10*365*24*60*60*1000));
     document.cookie = "theme=" + theme + ";expires=" + expiry_date.toUTCString() + ";path=/";
-    location.reload();
+    document.getElementById("stylesheet-theme").href = "/assets/common/css/" + theme + ".css"
 }
 
 function previousPage() {
